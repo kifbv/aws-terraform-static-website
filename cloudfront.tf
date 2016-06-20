e8099a7cedf2e8e9218881864c6f156df45fc581
Franck Ratier <franck@brandwatch.com>
2016-06-20 18:26:40 +0100

    add vars for some cloudfront distribution parameters

--- cloudfront.tf
+++ cloudfront.tf
@@ -39,8 +39,8 @@ resource "aws_cloudfront_distribution" "s3_distribution_blue" {
 
   # cache behaviour will be common to all websites
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods  = ["${split(",", var.cache_allowed_methods)}"]
+    cached_methods   = ["${split(",", var.cache_cached_methods)}"]
     target_origin_id = "${element(split(",", var.domain_names), count.index)}_${var.origin_suffix}"
     compress         = true
 
@@ -63,7 +63,7 @@ resource "aws_cloudfront_distribution" "s3_distribution_blue" {
   restrictions {
     geo_restriction {
       restriction_type = "${var.geo_restriction_type}"
-      locations        = [""]
+      locations        = ["${split(",", var.geo_restriction_locations)}"]
     }
   }
 
@@ -104,8 +104,8 @@ resource "aws_cloudfront_distribution" "s3_distribution_green" {
 
   # cache behaviour will be common to all websites
   default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods  = ["${split(",", var.cache_allowed_methods)}"]
+    cached_methods   = ["${split(",", var.cache_cached_methods)}"]
     target_origin_id = "${element(split(",", var.domain_names), count.index)}_${var.origin_suffix}"
     compress         = true
 
@@ -128,7 +128,7 @@ resource "aws_cloudfront_distribution" "s3_distribution_green" {
   restrictions {
     geo_restriction {
       restriction_type = "${var.geo_restriction_type}"
-      locations        = [""] 
+      locations        = ["${split(",", var.geo_restriction_locations)}"]
     }
   }
 
